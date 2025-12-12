88266da3988623b17936d59e98370a2bec42ae64
Viet Ho <viethdd@gmail.com>
2025-12-12 16:53:55 +0700

    fix intro page, add vercel tracking: part_1

---
+++ extra_javascript/vercel_track.js
@@ -0,0 +1,5 @@
+<script>
+  window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+</script>
+<script defer src="https://cdn.vercel-insights.com/v1/script.js">
+</script>
